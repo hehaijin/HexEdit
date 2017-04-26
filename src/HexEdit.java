e8cf1bcda2d7bde90299126154a2c28f7ad9ea57
hehaijin <hehaijin1981@gmail.com>
2017-04-26 16:55:43 -0600

    original

---
+++ src/HexEdit.java
@@ -0,0 +1,43 @@
+import java.io.File;
+
+public class HexEdit
+{
+  
+  public static void editFromRegistry(File f){
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  }
+  
+  
+  
+  
+  
+
+  public static void main(String[] args)
+  {
+    // TODO Auto-generated method stub
+    if (args[0] == null)
+    {
+
+      System.out.println("you need to specify the file to do hex editing");
+
+    }
+    
+    
+    
+    
+    
+    
+    
+
+  }
+
+}
